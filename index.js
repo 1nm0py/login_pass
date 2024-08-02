b810ce80d0cd68bb358539de20144564ec51a1c8
Inamov Asilbek <playerallgames007@gmail.cpm>
2024-08-02 20:55:40 +0500

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonSever = require("json-server");
 const server = jsonSever.create();
 const router = jsonSever.router("db.json");
 const middlewares = jsonSever.defaults();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 1080;
 
 server.use(middlewares);
 server.use(router);
